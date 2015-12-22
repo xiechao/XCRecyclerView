2c5737a9104dd7a9bf7336fa2143906a6fd62a6b
XieChao <xc9898@gmail.com>
2015-12-22 10:33:12 +0800

    init

--- app/src/main/java/com/xclib/recyclerviewtest/model/Person.java
+++ app/src/main/java/com/xclib/recyclerviewtest/model/Person.java
@@ -8,20 +8,12 @@ public class Person implements ISectionData {
     private String name;
     private String sectionHeader;
 
-    public Person() {
-
-    }
-
     public Person(String name) {
         this.name = name;
     }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     @Override

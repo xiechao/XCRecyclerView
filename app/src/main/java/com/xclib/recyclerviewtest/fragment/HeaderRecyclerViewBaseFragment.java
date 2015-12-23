a7e1177dd2a6023554c0ac874bbe529d19b25e36
XieChao <xc9898@gmail.com>
2015-12-23 15:47:09 +0800

    init

--- app/src/main/java/com/xclib/recyclerviewtest/fragment/HeaderRecyclerViewBaseFragment.java
+++ app/src/main/java/com/xclib/recyclerviewtest/fragment/HeaderRecyclerViewBaseFragment.java
@@ -1,104 +1,111 @@
 package com.xclib.recyclerviewtest.fragment;
 
 import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
-public interface HeaderViewProvider {
-    int getHeaderViewHeight();
+import com.xclib.recyclerview.EmptyView;
+import com.xclib.recyclerview.HeaderRecyclerViewAdapter;
+import com.xclib.recyclerview.RecyclerViewBaseAdapter;
+import com.xclib.recyclerview.XCRecycleView;
+import com.xclib.recyclerviewtest.R;
 
-    void onHeadScrollHScrolled(int scrollY);
-}}
+public abstract class HeaderRecyclerViewBaseFragment extends Fragment {
+
+    protected XCRecycleView xcRecycleView;
+    protected EmptyView emptyView;
+    private int scrollXValue = 0;
+    private int scrollYValue = 0;
+    private final RecyclerView.OnScrollListener onScrollListener = new RecyclerView.OnScrollListener() {
+
+        @Override
+        public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+            super.onScrolled(recyclerView, dx, dy);
+
+            scrollXValue += dx;
+            scrollYValue += dy;
+
+
+            if (getActivity() instanceof HeaderViewProvider) {
+                HeaderViewProvider headerViewProvider = (HeaderViewProvider) getActivity();
+
+                if (scrollYValue < headerViewProvider.getHeaderViewHeight() + 400) {
+                    headerViewProvider.onHeadScrollHScrolled(scrollYValue);
+                }
+            }
         }
-        };
+    };
+    private LinearLayout headerView;
+    private RecyclerViewBaseAdapter baseAdapter;
+    private RecyclerViewBaseAdapter adapter;
+    private final RecyclerView.AdapterDataObserver adapterDataObserver = new RecyclerView.AdapterDataObserver() {
+        @Override
+        public void onChanged() {
+            super.onChanged();
 
-protected XCRate ieView xcRecycleView;
-        proteate in extends Fragment{
-
-        priv nt scrollXValue=0;
-        privecyclt scrollYValue=0;
-
-private final RecyclerView.OnScrollListener onScrollListener=new RecyclerView.OnScrollListener(){
-
-@Override
-public void onScrolled(RecyclerView recyclerView,int dx,int dy){
-        super.onScrolled(recyclerView,dx,dy);
-
-        scrollXValue+=dx;
-        scrollYValue+=dy;
-
-
-        if(getActivity()instanceof HeaderViewProvider){
-        HeaderViewProvider headerViewProvider=(HeaderViewProvider)getActivity();
-
-        if(scrollYValue<headerViewProvider.getHeaderViewHeight()+400){
-        headerViewProvider.onHeadScrollHScrolled(scrollYValue);
-        }
-        cted EmptyView emptyView;
-private LinearLayout headerView;
-private RecyclerViewBaseAdapter baseAdapter;
-private RecyclerViewBaseAdapter adapter;
-private final RecyclerView.AdapterDataObserver adapterDataObserver=new RecyclerView.AdapterDataObserver(){
-@Override
-public void onChanged(){
-        super.onChanged();
-
-        adapter.notifyDataSetChanged();
-        }
-
-@Override
-public void onItemRangeChanged(int positionStart,int itemCount){
-        super.onItemRangeChanged(positionStart,itemCount);
-
-        adapter.notifyItemRangeChanged(positionStart+1,itemCount);
-        }
-
-@Override
-public void onItemRangeChanged(int positionStart,int itemCount,Object payload){
-        super.onItemRangeChanged(positionStart,itemCount,payload);
-
-        adapter.notifyItemRangeChanged(positionStart+1,itemCount);
+            adapter.notifyDataSetChanged();
         }
 
-@Override
-public void onItemRangeInserted(int positionStart,int itemCount){
-        super.onItemRangeInserted(positionStart,itemCount);
+        @Override
+        public void onItemRangeChanged(int positionStart, int itemCount) {
+            super.onItemRangeChanged(positionStart, itemCount);
 
-        adapter.notifyItemRangeInserted(positionStart+1,itemCount);
+            adapter.notifyItemRangeChanged(positionStart + 1, itemCount);
         }
 
-@Override
-public void onItemRangeRemoved(int positionStart,int itemCount){
-        super.onItemRangeRemoved(positionStart,itemCount);
+        @Override
+        public void onItemRangeChanged(int positionStart, int itemCount, Object payload) {
+            super.onItemRangeChanged(positionStart, itemCount, payload);
 
-        adapter.notifyItemRangeRemoved(positionStart+1,itemCount);
+            adapter.notifyItemRangeChanged(positionStart + 1, itemCount);
         }
 
-@Override
-public void onItemRangeMoved(int fromPosition,int toPosition,int itemCount){
-        super.onItemRangeMoved(fromPosition,toPosition,itemCount);
+        @Override
+        public void onItemRangeInserted(int positionStart, int itemCount) {
+            super.onItemRangeInserted(positionStart, itemCount);
 
-        adapter.notifyItemRangeRemoved(fromPosition+1,itemCount);
-        }
-        };
-
-@SuppressLint("CutPasteId")
-@Override
-public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState){
-        View rootView=inflater.inflate(R.layout.fragment_recycler_view_common,container,false);
-        xcRecycleView=(XCRecycleView)rootView.findViewById(R.id.xc_recycler_view);
-
-        emptyView=new EmptyView(getContext());
-
-        headerView=new LinearLayout(getContext());
-
-        if(getActivity()instanceof HeaderViewProvider){
-        HeaderViewProvider headerViewProvider=(HeaderViewProvider)getActivity();
-
-        View view=new View(getContext());
-        headerView.addView(view,new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,headerViewProvider.getHeaderViewHeight()));
+            adapter.notifyItemRangeInserted(positionStart + 1, itemCount);
         }
 
-        baseAdapter=getAdapter();
-        adapter=new HeaderRecyclerViewAdapter(getContext(),baseAdapter,headerView,emptyView);
+        @Override
+        public void onItemRangeRemoved(int positionStart, int itemCount) {
+            super.onItemRangeRemoved(positionStart, itemCount);
+
+            adapter.notifyItemRangeRemoved(positionStart + 1, itemCount);
+        }
+
+        @Override
+        public void onItemRangeMoved(int fromPosition, int toPosition, int itemCount) {
+            super.onItemRangeMoved(fromPosition, toPosition, itemCount);
+
+            adapter.notifyItemRangeRemoved(fromPosition + 1, itemCount);
+        }
+    };
+
+    @SuppressLint("CutPasteId")
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_recycler_view_common, container, false);
+        xcRecycleView = (XCRecycleView) rootView.findViewById(R.id.xc_recycler_view);
+
+        emptyView = new EmptyView(getContext());
+
+        headerView = new LinearLayout(getContext());
+
+        if (getActivity() instanceof HeaderViewProvider) {
+            HeaderViewProvider headerViewProvider = (HeaderViewProvider) getActivity();
+
+            View view = new View(getContext());
+            headerView.addView(view, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, headerViewProvider.getHeaderViewHeight()));
+        }
+
+        baseAdapter = getAdapter();
+        adapter = new HeaderRecyclerViewAdapter(getContext(), baseAdapter, headerView, emptyView);
 
         registerAdapterDataObserver();
 
@@ -109,10 +116,10 @@ public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle save
         xcRecycleView.addOnScrollListener(onScrollListener);
 
         return rootView;
-        }
+    }
 
-@Override
-public void onDestroy(){
+    @Override
+    public void onDestroy() {
         super.onDestroy();
 
         unregisterAdapterDataObserver();
@@ -120,21 +127,25 @@ public void onDestroy(){
         xcRecycleView.removeOnScrollListener(onScrollListener);
 
         xcRecycleView.clear();
-        }
+    }
 
-protected abstract RecyclerViewBaseAdapter getAdapter();
+    protected abstract RecyclerViewBaseAdapter getAdapter();
 
-protected abstract RecyclerView.LayoutManager getLayoutManager();
+    protected abstract RecyclerView.LayoutManager getLayoutManager();
 
-private void registerAdapterDataObserver(){
+    private void registerAdapterDataObserver() {
         baseAdapter.registerAdapterDataObserver(adapterDataObserver);
-        }
+    }
 
 
-private void unregisterAdapterDataObserver(){
+    private void unregisterAdapterDataObserver() {
         baseAdapter.unregisterAdapterDataObserver(adapterDataObserver);
-        }
+    }
 
-public abstract class HeaderRecyclerViewBaseFragment
+    public interface HeaderViewProvider {
+        int getHeaderViewHeight();
+
+        void onHeadScrollHScrolled(int scrollY);
+    }
 
 }

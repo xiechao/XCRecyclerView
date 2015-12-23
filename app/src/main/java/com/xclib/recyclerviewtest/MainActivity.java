6ae461f63c8e5da89b85716d9b5c47473794e810
XieChao <xc9898@gmail.com>
2015-12-23 09:43:18 +0800

    init

--- app/src/main/java/com/xclib/recyclerviewtest/MainActivity.java
+++ app/src/main/java/com/xclib/recyclerviewtest/MainActivity.java
@@ -6,30 +6,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.xclib.recyclerviewtest.activities.PinnedSectionActivity;
-import com.xclib.recyclerviewtest.activities.PinnedSectionSwipeActivity;
 import com.xclib.recyclerviewtest.activities.GridLayoutManagerActivity;
 import com.xclib.recyclerviewtest.activities.LinearLayoutManagerActivity;
 import com.xclib.recyclerviewtest.activities.LinearLayoutManagerSwipeActivity;
 import com.xclib.recyclerviewtest.activities.ObservableRecyclerViewActivity;
+import com.xclib.recyclerviewtest.activities.PinnedSectionActivity;
+import com.xclib.recyclerviewtest.activities.PinnedSectionSwipeActivity;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+
     @Bind(R.id.btn_linear_layout_manager)
     Button btnLinearLayoutManager;
     @Bind(R.id.btn_relative_layout_manager)
     Button btnRelativeLayoutManager;
-    @Bind(R.id.btn_header_section)
-    Button btnHeaderSection;
+    @Bind(R.id.btn_pinned_section)
+    Button btnPinnedSection;
     @Bind(R.id.btn_linear_layout_swipe_manager)
     Button btnLinearLayoutSwipeManager;
-    @Bind(R.id.btn_header_section_swipe)
-    Button btnHeaderSectionSwipe;
-    @Bind(R.id.btn_observable_scroll)
-    Button btnObservableScroll;
+    @Bind(R.id.btn_pinned_section_swipe)
+    Button btnPinnedSectionSwipe;
+    @Bind(R.id.btn_observable_recycler_view)
+    Button btnObservableRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,8 +54,8 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @OnClick({R.id.btn_header_section})
-    public void onClickHeaderSection(View view) {
+    @OnClick({R.id.btn_pinned_section})
+    public void onClickPinnedSection(View view) {
         Intent intent = new Intent(this, PinnedSectionActivity.class);
 
         startActivity(intent);
@@ -66,15 +68,15 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
-    @OnClick({R.id.btn_header_section_swipe})
-    public void onClickHeaderSectionSwipe(View view) {
+    @OnClick({R.id.btn_pinned_section_swipe})
+    public void onClickPinnedSectionSwipe(View view) {
         Intent intent = new Intent(this, PinnedSectionSwipeActivity.class);
 
         startActivity(intent);
     }
 
-    @OnClick({R.id.btn_observable_scroll})
-    public void onClickObservableScroll(View view) {
+    @OnClick({R.id.btn_observable_recycler_view})
+    public void onClickObservableRecyclerView(View view) {
         Intent intent = new Intent(this, ObservableRecyclerViewActivity.class);
 
         startActivity(intent);
